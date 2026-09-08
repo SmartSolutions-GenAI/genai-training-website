f4323564e0c580427902c09794f173cacb196826
SmartSolutions-GenAI <smartsolutions4567@gmail.com>
2026-09-08 17:40:29 +0530

    Initial commit

--- src_ver1/components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-export default function Banner() {
-  return (
-    <div className="banner">
-      <div className="wrap">
-        <h2>Python developers who build AI will lead the next wave.</h2>
-      </div>
-    </div>
-  )
-}
